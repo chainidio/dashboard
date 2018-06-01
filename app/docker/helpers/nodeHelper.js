8ebe7a83fcc608fb06898dc2102f47befe215683
Chain ID Project <37491678+chainidio@users.noreply.github.com>
2018-06-01 23:35:28 +0700

    Add files via upload

---
+++ app/docker/helpers/nodeHelper.js
@@ -0,0 +1,14 @@
+angular.module('chainid.docker')
+.factory('NodeHelper', [function NodeHelperFactory() {
+  'use strict';
+  return {
+    nodeToConfig: function(node) {
+      return {
+        Name: node.Spec.Name,
+        Role: node.Spec.Role,
+        Labels: node.Spec.Labels,
+        Availability: node.Spec.Availability
+      };
+    }
+  };
+}]);
