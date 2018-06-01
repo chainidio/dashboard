e4842ca529f758a7fc31e8a7d102620fa87ebca9
Chain ID Project <37491678+chainidio@users.noreply.github.com>
2018-06-01 23:22:40 +0700

    Add files via upload

---
+++ app/agent/components/node-selector/node-selector.js
@@ -0,0 +1,7 @@
+angular.module('chainid.agent').component('nodeSelector', {
+  templateUrl: 'app/agent/components/node-selector/nodeSelector.html',
+  controller: 'NodeSelectorController',
+  bindings: {
+    model: '='
+  }
+});
---
+++ app/azure/components/azure-sidebar-content/azure-sidebar-content.js
@@ -0,0 +1,3 @@
+angular.module('chainid.azure').component('azureSidebarContent', {
+  templateUrl: 'app/azure/components/azure-sidebar-content/azureSidebarContent.html'
+});
